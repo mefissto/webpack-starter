b7e3672cc0671a6603a07aab96695c64ca26e683
mefissto <mefissto.w@gmail.com>
2019-07-07 23:12:02 +0300

    updated webpack config

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,14 +1,25 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../dist'),
+  assets: 'assets/'
+};
 
 module.exports = {
+  externals: {
+    paths: PATHS
+  },
   entry: {
-    app: './src/index.js'
+    app: PATHS.src
   },
   output: {
-    filename: '[name].js', // 'name' will take from the entry item name (here --> app)
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist' // for webpack-dev-server
+    filename: `${PATHS.assets}js/[name].js`, // 'name' will take from the entry item name (here --> app)
+    path: PATHS.dist,
+    publicPath: '/' // for webpack-dev-server
   },
   module: {
     rules: [
@@ -16,6 +27,13 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
       },
       {
         test: /\.scss$/, // --> /\.(sa|sc|c)ss$/
@@ -59,12 +77,24 @@ module.exports = {
       }
     ]
   },
-  devServer: {
-    overlay: true
-  },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: '[name].css'
-    })
+      filename: `${PATHS.assets}css/[name].css`
+    }),
+    new HtmlWebpackPlugin({
+      hash: false,
+      template: `${PATHS.src}/index.html`,
+      filename: './index.html'
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: `${PATHS.src}/img`,
+        to: `${PATHS.assets}img`
+      },
+      {
+        from: `${PATHS.src}/static`,
+        to: ''
+      }
+    ])
   ]
 };
