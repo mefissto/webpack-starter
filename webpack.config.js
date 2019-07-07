42f5cca9683efea47b42d7df8e80f443fde563c5
mefissto <mefissto.w@gmail.com>
2019-07-07 22:06:06 +0300

    added config for css and postcss plugins

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,44 @@ module.exports = {
         exclude: '/node_modules/'
       },
       {
+        test: /\.scss$/, // --> /\.(sa|sc|c)ss$/
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: { path: `./src/js/postcss.config.js` }
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ]
+      },
+      {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: { path: `./src/js/postcss.config.js` }
+            }
+          }
+        ]
       }
     ]
   },
